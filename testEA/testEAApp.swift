aa4173b1c92210e1f4ad4019185dd276918ea9ef
Gerald <geraldk675@gmail.com>
2023-01-18 17:12:03 +1100

    Initial Commit

---
+++ testEA/testEAApp.swift
@@ -0,0 +1,17 @@
+//
+//  testEAApp.swift
+//  testEA
+//
+//  Created by Gerald Kim on 18/1/2023.
+//
+
+import SwiftUI
+
+@main
+struct testEAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ testEAUITests/testEAUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  testEAUITestsLaunchTests.swift
+//  testEAUITests
+//
+//  Created by Gerald Kim on 18/1/2023.
+//
+
+import XCTest
+
+final class testEAUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
